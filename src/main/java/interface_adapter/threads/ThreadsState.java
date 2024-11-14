d1990dd690944385358d07026a26b75c7b975986
Benjamin <112658491+Benjamingavriely@users.noreply.github.com>
2024-11-14 16:30:12 -0500

    More checkstyle fixed

--- src/main/java/interface_adapter/threads/ThreadsState.java
+++ src/main/java/interface_adapter/threads/ThreadsState.java
@@ -1,7 +1,8 @@
 package interface_adapter.threads;
 
 /**
- * idk if we even need this because threads has no user input other than clicking, which is handled by the actionlistener anyway
+ * Idk if we even need this because threads
+ * has no user input other than clicking, which is handled by the actionlistener anyway.
  */
 public class ThreadsState {
 }
--- src/main/java/view/ButtonLabelPanel.java
+++ src/main/java/view/ButtonLabelPanel.java
@@ -1,6 +1,10 @@
 package view;
-import javax.swing.*;
+
 import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 
 /**
  * A panel containing a label, then a button.
@@ -9,6 +13,7 @@ public class ButtonLabelPanel extends JPanel
 {
     private final JButton button = new JButton("view");
     private JLabel label = new JLabel();
+
     ButtonLabelPanel(JLabel label)
     {
         this.label = label;
@@ -16,13 +21,19 @@ public class ButtonLabelPanel extends JPanel
         this.add(this.label);
     }
 
-    public JButton getButton()
+    /**
+     * This initialises an actionListener for the button in this panel.
+     * @param listener the actionlistener for this button.
+     */
+    public void setActionListener(ActionListener listener)
     {
-        return button;
-    }
-    public void setActionListener(ActionListener listener){
         button.addActionListener(listener);
     }
+
+    /**
+     * This returns the text content of the label.
+     * @return the contents of the label.
+     */
     public String getLabelContent()
     {
         return label.getText();
