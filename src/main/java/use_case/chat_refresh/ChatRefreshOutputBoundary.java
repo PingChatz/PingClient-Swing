16989b37ab8f59ae2cfe347e690dec8cbe29bfaa
Benjamin <112658491+Benjamingavriely@users.noreply.github.com>
2024-11-29 14:38:56 -0500

    Idek anymore

--- src/main/java/use_case/chat_refresh/ChatRefreshOutputBoundary.java
+++ src/main/java/use_case/chat_refresh/ChatRefreshOutputBoundary.java
@@ -1,12 +1,11 @@
 package use_case.chat_refresh;
 
-import use_case.logout.LogoutOutputData;
 
 public interface ChatRefreshOutputBoundary {
     /** Prepares the success view for the Login Use Case.
-     @param outputData the output data
+     * output data
      */
 
-    void prepareSuccessView(LogoutOutputData outputData);
+    void prepareSuccessView();
 
 }
