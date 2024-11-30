ebf5459b4fd08249d2240793b1afe786bf89d89c
Roppax <rob.reder06@gmail.com>
2024-11-30 12:43:45 -0500

    Merged 2 Interfaces into one GetThreadsThreadDataAccessInterface to remove redundancy and complication.

--- src/main/java/use_case/get_threads/GetThreadsThreadDataAccessInterface.java
+++ src/main/java/use_case/get_threads/GetThreadsThreadDataAccessInterface.java
@@ -1,7 +1,19 @@
 package use_case.get_threads;
 
+import entity.Thread;
 import java.util.List;
 
 public interface GetThreadsThreadDataAccessInterface {
-    List<Long> getUserThreadIDs(Long userID); // get all thread ids for user
+    /**
+     * Fetch all thread IDs associated with the given user.
+     * @param userID The ID of the user.
+     * @return List of thread IDs.
+     */
+    List<Long> getUserThreadIDs(Long userID);
+
+    /**
+     * Fetch all threads for the current user.
+     * @return List of threads.
+     */
+    List<Thread> getThreads();
 }
